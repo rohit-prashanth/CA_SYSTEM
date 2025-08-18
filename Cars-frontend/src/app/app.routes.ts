f6dc8a561f5aefac08bbcaf6b3e347dfe63c3dd9
Rohit Prashant Kannam <XYRE00446@coromandel.murugappa.com>
2025-08-18 18:03:17 +0530

    changes w.r.t font sizes, alignments

--- Cars-frontend/src/app/app.routes.ts
+++ Cars-frontend/src/app/app.routes.ts
@@ -7,53 +7,80 @@ import { RequestDetail } from './components/request-detail/request-detail';
 import { DocumentEditor } from './components/document-editor/document-editor';
 import { MainLayout } from './components/main-layout/main-layout';
 
+
 export const routes: Routes = [
   {
     path: '',
-    component: Dashboard,
+    component: MainLayout, // This wraps all your main components
     children: [
+      { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+      { path: 'dashboard', component: Dashboard },
       { path: 'create-request', component: CreateRequest },
-      { path: 'main-layout', component: MainLayout },
-      { path: '', pathMatch: 'full', redirectTo: 'main-layout' }, // shows empty by default
       {
         path: 'view-request',
-        data: { breadcrumb: 'View Request' },
         component: ViewRequest,
         children: [
-          {
-            path: 'requests/:requestId',
-            data: { breadcrumb: 'Request :requestId' },
-            component: RequestDetail,
-          },
+          { path: 'requests/:requestId', component: RequestDetail },
           {
             path: 'requests/:requestId/sections/:sectionId/subsections/:subsectionId',
-            data: { breadcrumb: 'Subsection :subsectionId' },
             component: SubsectionDetail,
           },
         ],
       },
-    ],
-  },
-  // {
-  //   path: '',
-  //   component: MainLayout,
-  //   children: [
-  //     { path: '', redirectTo: 'create-request', pathMatch: 'full' },
-  //     { path: 'create-request', component: CreateRequest },
-  //     { path: 'dashboard', component: Dashboard},
-  //     // add other pages here
-  //   ],
-  // },
-
-  // { path: '', data: { breadcrumb: 'Dashboard' }, component: Dashboard },
-  // {
-  //   path: 'create-request',
-  //   data: { breadcrumb: 'Create Request' },
-  //   component: CreateRequest,
-  // },
-
-  { path: 'editor', component: DocumentEditor },
+      { path: 'editor', component: DocumentEditor }
+    ]
+  }
 ];
+
+
+
+// export const routes: Routes = [
+//   {
+//     path: '',
+//     component: Dashboard,
+//     children: [
+//       { path: 'create-request', component: CreateRequest },
+//       { path: 'main-layout', component: MainLayout },
+//       { path: '', pathMatch: 'full', redirectTo: 'main-layout' }, // shows empty by default
+//       {
+//         path: 'view-request',
+//         // data: { breadcrumb: 'View Request' },
+//         component: ViewRequest,
+//         children: [
+//           {
+//             path: 'requests/:requestId',
+//             // data: { breadcrumb: 'Request :requestId' },
+//             component: RequestDetail,
+//           },
+//           {
+//             path: 'requests/:requestId/sections/:sectionId/subsections/:subsectionId',
+//             // data: { breadcrumb: 'Subsection :subsectionId' },
+//             component: SubsectionDetail,
+//           },
+//         ],
+//       },
+//     ],
+//   },
+//   // {
+//   //   path: '',
+//   //   component: MainLayout,
+//   //   children: [
+//   //     { path: '', redirectTo: 'create-request', pathMatch: 'full' },
+//   //     { path: 'create-request', component: CreateRequest },
+//   //     { path: 'dashboard', component: Dashboard},
+//   //     // add other pages here
+//   //   ],
+//   // },
+
+//   // { path: '', data: { breadcrumb: 'Dashboard' }, component: Dashboard },
+//   // {
+//   //   path: 'create-request',
+//   //   data: { breadcrumb: 'Create Request' },
+//   //   component: CreateRequest,
+//   // },
+
+//   { path: 'editor', component: DocumentEditor },
+// ];
 
 // import { Routes } from '@angular/router';
 // import { Dashboard } from './components/dashboard/dashboard';
