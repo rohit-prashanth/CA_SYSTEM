0e4d9cb565205596db5829f1096f78550894bad3
Rohit Prashant Kannam <XYRE00446@coromandel.murugappa.com>
2025-08-18 18:03:17 +0530

    changes w.r.t font sizes, alignments

--- Cars-frontend/src/app/components/dashboard/dashboard.ts
+++ Cars-frontend/src/app/components/dashboard/dashboard.ts
@@ -1,13 +1,35 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router'; 
+import { Router, NavigationEnd, RouterOutlet, RouterLink } from '@angular/router'; // Import Router and NavigationEnd
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet],
   templateUrl: './dashboard.html',
-  styleUrl: './dashboard.css'
+  styleUrl: './dashboard.css',
 })
 export class Dashboard {
-
+  
 }
+
+
+
+
+
+// import { Component } from '@angular/core';
+// import { RouterLink, RouterOutlet } from '@angular/router';
+
+// @Component({
+//   selector: 'app-dashboard',
+//   standalone: true,
+//   imports: [RouterOutlet, RouterLink],
+//   templateUrl: './dashboard.html',
+//   styleUrl: './dashboard.css',
+// })
+// export class Dashboard {
+//   isCollapsed = false;
+
+//   toggleSidebar() {
+//     this.isCollapsed = !this.isCollapsed;
+//   }
+// }
